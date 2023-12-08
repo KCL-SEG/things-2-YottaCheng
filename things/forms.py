8e247e4162053d3e64e0f6ce87fbc3881f294300
Ziyao Cheng <yottacheng3@gmail.com>
2023-12-08 00:52:22 +0000

    new

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,6 @@
-"""Forms of the project."""
+from django import forms
 
-# Create your forms here.
+class ThingForm(forms.Form):
+    name = forms.CharField(max_length=30)
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
--- things/views.py
+++ things/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+from django import forms  # 添加这一行以正确导入 forms 模块
+
+
+
 def home(request):
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
+
+
+
+
+class ThingForm(forms.Form):
+    name = forms.CharField(max_length=30)
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput)
