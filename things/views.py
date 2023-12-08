cfe1014f271d624d083aac4155aa3e2dd1b6a4a9
Ziyao Cheng <yottacheng3@gmail.com>
2023-12-08 00:57:18 +0000

    new

--- things/views.py
+++ things/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ThingForm
 
 from django import forms  # 添加这一行以正确导入 forms 模块
 
@@ -12,6 +13,7 @@ def home(request):
 
 
 class ThingForm(forms.Form):
-    name = forms.CharField(max_length=30)
-    description = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(widget=forms.Textarea(attrs={'maxlength': '120'}))  # 设置 description 字段的最大长度为 120，同时使用 Textarea 将其渲染成多行文本框
     quantity = forms.IntegerField(widget=forms.NumberInput)
+
